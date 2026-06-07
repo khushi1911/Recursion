043fa3b344c694813d2acacde000c314439fcb37
khushi1911 <khushikaushik2030@gmail.com>
2026-06-07 23:16:21 +0530

    factorial of a no.

---
+++ factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class factorial{
+
+    public static int fact(int n){
+        
+        if(n==0 || n==1){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println("enter the no:");
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fact(n));
+    }
+}
