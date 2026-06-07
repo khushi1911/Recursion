d9c55a9515e48d6472c9bc2057e88cd1c8a229b0
khushi1911 <khushikaushik2030@gmail.com>
2026-06-07 23:25:28 +0530

    fibonacci series

---
+++ fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class fibonacci{
+
+    public static int fib(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("enter the no:");
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fib(n));
+    }
+}
